af1703ff4bc77d4fe2bd79f3386aa854f24237cf
CamronGlassmann <100964567+CamronGlassmann@users.noreply.github.com>
2022-04-22 16:42:51 -0500

    interfaces

---
+++ Azul-Pizza/src/app/pizza.ts
@@ -0,0 +1,7 @@
+export interface Pizza {
+    name: string;
+    toppings: string []
+    sause: string;
+    cheese: string [];
+    crust: string;
+  }
---
+++ Azul-Pizza/src/app/premadePizzaList.ts
@@ -0,0 +1,5 @@
+import {Pizza} from './pizza'
+export const PREMADES: Pizza[] = [
+    {name:'3Cheese',toppings: [''], cheese :['m','other cheese','cheese'],sause: 'maranara',crust:'basic'},
+    {name:'meatLover',toppings: ['chichen','sauage','ham','beef'], cheese :['cheese'],sause: 'maranara',crust:'basic'}
+];
