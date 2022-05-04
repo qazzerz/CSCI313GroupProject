dcfceaaa04b86b12143f0bbf2494154b63b95827
CamronGlassmann <100964567+CamronGlassmann@users.noreply.github.com>
2022-05-04 13:09:42 -0500

    otherMenuItems

---
+++ Azul-Pizza/src/app/OtherMenuItems.ts
@@ -0,0 +1,14 @@
+export interface drinks{
+    price: number;
+    name:string;
+    img: string;
+}
+export const OTHERMENU: any[] = [
+    {price: 2,name: "Coca Cola",img:""},
+    {price: 2,name: "Mountain Dew",img:""},
+    {price: 2,name: "Sprite",img:""},
+    {price: 2,name: "Dr Pepper",img:""},
+    {price: 2,name: "Root bear",img:""},
+    {price: 1,name: "Water",img:""},
+    {price: 2,name: "Cherry Coca Cola",img:""},
+] 
