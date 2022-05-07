d6d32bed3da18690c426d40ea6aa961dde901a99
JakeACollins <100799909+JakeACollins@users.noreply.github.com>
2022-05-07 15:10:12 -0500

    cunble to cart

--- Azul-Pizza/src/app/OtherMenuItems.ts
+++ Azul-Pizza/src/app/OtherMenuItems.ts
@@ -1,14 +1,15 @@
 export interface drinks{
+    id: number;
     price: number;
     name:string;
     img: string;
 }
 export const OTHERMENU: any[] = [
-    {price: 2,name: "Coca Cola",img:""},
-    {price: 2,name: "Mountain Dew",img:""},
-    {price: 2,name: "Sprite",img:""},
-    {price: 2,name: "Dr Pepper",img:""},
-    {price: 2,name: "Root bear",img:""},
-    {price: 1,name: "Water",img:""},
-    {price: 2,name: "Cherry Coca Cola",img:""},
+    {id: 0, price: 2,name: "Coca Cola",img:""},
+    {id: 1, price: 2,name: "Mountain Dew",img:""},
+    {id: 2, price: 2,name: "Sprite",img:""},
+    {id: 3, price: 2,name: "Dr Pepper",img:""},
+    {id: 4, price: 2,name: "Root bear",img:""},
+    {id: 5, price: 1,name: "Water",img:""},
+    {id: 6, price: 2,name: "Cherry Coca Cola",img:""},
 ] 
--- Azul-Pizza/src/app/main-page/bundle/bundle.component.ts
+++ Azul-Pizza/src/app/main-page/bundle/bundle.component.ts
@@ -21,48 +21,54 @@ export class BundleComponent implements OnInit {
   @Input() tempPizzaPrice: number = 0;
   @Input() tempPizzaList: Pizza[] = PREMADES;
   
-  updatedPriceValue: number = 0;
+  updatedDoublePriceId: number = 0;
   pricedOut: number = 0;
   pricedOutDouble: number = 0;
   tempPizzaId: number = 0;
   tempPizzaDouble1Id: number = 0;
   tempPizzaDouble2Id: number = 0;
-
+  doublePopChoice: string = "";
+  updatedSinglePriceId: number = 0;
     constructor(private shopSer: ShoppingCartService) { }
   
     ngOnInit(): void {
-      this.pricedOut = this.tempPizzaList[0].price;
+      this.pricedOut = +this.tempPizzaList[0].price + +this.otherMenu[0].price;
       this.tempPizza = this.tempPizzaList[0];
       this.tempPizzaDoubleList[0] = this.tempPizzaList[0]
       this.tempPizzaDoubleList[1] = this.tempPizzaList[0]
       this.pricedOutDouble = this.tempPizzaDoubleList[0].price + this.tempPizzaDoubleList[1].price;
     }
     ngOnChanges(){
-      this.tempPizza;
+      
     }
     updatePizzaDouble(){
       this.tempPizzaDoubleList[0] = this.tempPizzaList[this.tempPizzaDouble1Id];
       this.tempPizzaDoubleList[1] = this.tempPizzaList[this.tempPizzaDouble2Id];
+      this.doublePopChoice = this.otherMenu[this.updatedDoublePriceId].name;
       if(this.tempPizzaDoubleList.length = 2){
-        this.pricedOutDouble = +this.updatedPriceValue + +this.tempPizzaDoubleList[0].price + +this.tempPizzaDoubleList[1].price - 2;
+        this.pricedOutDouble = +this.otherMenu[this.updatedDoublePriceId].price + +this.tempPizzaDoubleList[0].price + +this.tempPizzaDoubleList[1].price - 2;
       }
       else{
-      this.pricedOutDouble = +this.updatedPriceValue + +this.tempPizzaDoubleList[0].price + +this.tempPizzaDoubleList[1].price;
+      this.pricedOutDouble = +this.otherMenu[this.updatedDoublePriceId].price + +this.tempPizzaDoubleList[0].price + +this.tempPizzaDoubleList[1].price;
       }
     }
 
-
+    updateDoublePrice(){
+      this.pricedOut = +this.otherMenu[this.updatedDoublePriceId].price + +this.tempPizza.price;
+    }
     updatePizza(){
       this.tempPizza = this.tempPizzaList[this.tempPizzaId];
-      this.pricedOut = +this.updatedPriceValue + +this.tempPizza.price;
+      this.pricedOut = +this.otherMenu[this.tempPizzaId].price + +this.tempPizza.price;
     }
     updatePrice(){
-      this.pricedOut = +this.updatedPriceValue + +this.tempPizza.price;
+      this.pricedOut = +this.otherMenu[this.updatedSinglePriceId].price + +this.tempPizza.price;
     }
-    addPizza(tempBundle: any = {name: this.tempPizza.name, price: this.tempPizza.price }){
-      var price: number = +this.updatedPriceValue + +this.tempPizza.price;
-      var tempName: string = tempBundle.name;
-      console.log(price);
-      this.shopSer.addItem({price , tempName});
+    addPizzaPop(){
+      var desciption: string = this.tempPizza.name + ", " + this.otherMenu[this.updatedDoublePriceId].name;
+      this.shopSer.addItem({desciption: desciption , price: this.pricedOut, name: "Pizza Pop Bundle: "});
+    }
+    addDoublePizza(){
+      var desciption: string = this.tempPizzaDoubleList[0].name + ", " + this.tempPizzaDoubleList[1].name + ", " + this.otherMenu[this.updatedDoublePriceId].name;
+      this.shopSer.addItem({desciption: desciption , price: this.pricedOutDouble, name: "2 Pizza Bundle: "});
     }
   }
