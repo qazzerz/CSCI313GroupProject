fa9aab43076e5136b9ed9d7af7ecd796de2948ce
JakeACollins <100799909+JakeACollins@users.noreply.github.com>
2022-05-07 00:32:07 -0500

    q

--- Azul-Pizza/src/app/main-page/bundle/bundle.component.ts
+++ Azul-Pizza/src/app/main-page/bundle/bundle.component.ts
@@ -11,27 +11,31 @@ import { from } from 'rxjs';
   styleUrls: ['./bundle.component.css']
 })
 export class BundleComponent implements OnInit {
-  @Input() tempPizza: Pizza = {name:'',toppings: [], cheese :'',sauce: ''
-  ,crust:'', img: '', price: 0, desciption:'',fanFav : false};
+  @Input() tempPizza: Pizza = {
+    name: '', toppings: [], cheese: '', sauce: ''
+    , crust: '', img: '', price: 0, desciption: '', fanFav: false
+  };
   @Input() otherMenu: any[] = OTHERMENU;
   @Input() tempPizzaPrice: number = 0;
-  
-  updatedPriceValue: number = 0;
 
-    constructor(private shopSer: ShoppingCartService) { }
-  
-    ngOnInit(): void {
-    }
-    
-    updatePrice(){
-      let x:number = this.tempPizzaPrice, y = this.tempPizza.price
-      if(x = y){
+  updatedPriceValue: number = 0;
+  updatedPriceValue1: number = 0;
+
+  constructor(private shopSer: ShoppingCartService) { }
+
+  ngOnInit(): void {
+  }
+
+  updatePrice() {
+    let x: number = this.tempPizzaPrice, y = this.tempPizza.price
+    if (x < y) {
+      this.updatedPriceValue1 = this.updatedPriceValue;
       this.tempPizza.price = +this.updatedPriceValue + +this.tempPizza.price;
-      }
-    }
-    addPizza(tempBundle: any = {name: this.tempPizza.name, price: this.tempPizza.price }){
-      var price: number = tempBundle.price;
-      var tempName: string = tempBundle.name;
-      this.shopSer.addItem({price , tempName});
     }
   }
+  addPizza(tempBundle: any = { name: this.tempPizza.name, price: this.tempPizza.price }) {
+    var price: number = tempBundle.price;
+    var tempName: string = tempBundle.name;
+    this.shopSer.addItem({ price, tempName });
+  }
+}
